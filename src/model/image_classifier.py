d3389fb346412feba71749026ba0e113d65b2f78
AlexRaschl <Alexander.Raschl@aon.at>
2020-03-18 17:15:33 +0100

    Introduced Image Classification method that sums up all the predicted probability vectors of the Image Samples and normalizes them.

--- src/model/image_classifier.py
+++ src/model/image_classifier.py
@@ -1,3 +1,4 @@
+import os
 from itertools import product
 from os import listdir
 from os.path import join, isfile
@@ -10,8 +11,10 @@ from tensorflow.keras.applications.resnet import ResNet50
 from tensorflow.keras.applications.resnet50 import preprocess_input
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tqdm import tqdm
 
-from src.config import DL_PATH, N_SAMPLES, INDEXED_TTS_PATH
+from src.config import DL_PATH, N_SAMPLES, INDEXED_TTS_PATH, STORED_PRED_PATH
+from src.model.cfw import write_to_file
 from src.preprocessing.indexer import Indexer
 from src.preprocessing.wrappers import FramePredictions
 
@@ -33,7 +36,11 @@ class ImageClassifier:
             self.input_shape = (224, 224, 3)
             self.model = ResNet50(weights='imagenet')
 
-    def classify(self, X):
+    def classify(self, X: pd.DataFrame, aggregate: bool = False) -> pd.DataFrame:
+        return self.__compute_aggregated_predictions(X) if aggregate \
+            else self.__compute_full_predictions(X)
+
+    def __compute_full_predictions(self, X: pd.DataFrame) -> pd.DataFrame:
         predictions = np.empty(shape=(X.shape[0], self.batch_size),
                                dtype=np.dtype([
                                    ('predictions', np.int64, (self.n_predictions,)),
@@ -64,7 +71,22 @@ class ImageClassifier:
 
         df = pd.DataFrame(wrappers).set_index(pd.Index(X.iloc[:, 0]))
         df.columns = [f'frame_{s}' for s in range(self.batch_size)]
+        df.reset_index(inplace=True)
         del predictions
+        return df
+
+    def __compute_aggregated_predictions(self, X: pd.DataFrame) -> pd.DataFrame:
+        data = np.empty(shape=(X.shape[0], 1000))
+
+        for idx, v_id in enumerate(tqdm(X.iloc[:, 0])):
+            batch = self.load_batch(v_id)
+            predictions = self.model.predict(batch, batch_size=self.batch_size)
+            predictions = predictions.sum(axis=0)  # Sum all frames
+            predictions = predictions / predictions.sum(axis=0)  # Normalize probabilities
+            data[idx] = predictions
+
+        df = pd.DataFrame(data=data, index=X.iloc[:, 0])
+        df.reset_index(inplace=True)
         return df
 
     def load_batch(self, v_id: str):
@@ -88,8 +110,11 @@ if __name__ == '__main__':
     print(X_train.shape)
     print(X_test.shape)
     img_cf.init_model(model_name='ResNet50')
-    classifications = img_cf.classify(X_train)
-    # write_to_file(os.path.join(STORED_PRED_PATH, 'train_cnn_pred.pkl'), classifications)
 
-    classifications = img_cf.classify(X_test)
-    #write_to_file(os.path.join(STORED_PRED_PATH, 'test_cnn_pred.pkl'), classifications)
+    aggregate = False
+
+    classifications = img_cf.classify(X_train, aggregate)
+    write_to_file(os.path.join(STORED_PRED_PATH, f'train_cnn_agg_{str(aggregate)}.pkl'), classifications)
+
+    classifications = img_cf.classify(X_test, aggregate)
+    write_to_file(os.path.join(STORED_PRED_PATH, f'test_cnn_agg_{str(aggregate)}.pkl'), classifications)
