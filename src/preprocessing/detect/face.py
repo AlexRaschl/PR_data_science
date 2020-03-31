55fca3657948b92772c68443de3f932c022e861e
AlexRaschl <Alexander.Raschl@aon.at>
2020-03-31 19:43:12 +0200

    Added more face-detection features, like faces per frame and relative face sizes.

--- src/preprocessing/detect/face.py
+++ src/preprocessing/detect/face.py
@@ -19,30 +19,42 @@ class FaceDetector:
 
     def get_face_counts(self, X: pd.DataFrame):
         v_ids = DataManager.extract_v_ids(X)
-        n_faces = [self.detect_faces(v_id) for v_id in tqdm(v_ids)]
-        return pd.DataFrame({'v_id': v_ids, 'n_faces': n_faces})
+        face_info = np.array([self.detect_faces(v_id) for v_id in tqdm(v_ids)])
+        return pd.DataFrame({'v_id': v_ids,
+                             'n_faces': face_info[:, 0],
+                             'face_coverage': face_info[:, 1],
+                             'face_per_frame': face_info[:, 2]})
 
     def detect_faces(self, v_id: str):
         images = self.load_batch(v_id)
 
+        face_covered_area = 0.0
         n_faces = 0
         for img in images:
             faces = self.face_cascade.detectMultiScale(img,
                                                        scaleFactor=1.1,
                                                        minNeighbors=5,
                                                        minSize=(25, 25))
+
             n_faces += len(faces)
-        return n_faces
+            face_covered_area += self.get_coverage(faces, img)
+        return n_faces, face_covered_area / len(images), n_faces / len(images)
 
     def load_batch(self, v_id: str) -> List[np.ndarray]:
         return [cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY) for path in
                 natsorted(glob.glob(DataManager.sample_path_from_id(v_id) + '/*'))]
+
+    def get_coverage(self, faces, img):
+        total_area = 0
+        for (_, _, h, w) in faces:
+            total_area += h * w
+        return total_area / np.prod(img.shape)
 
 
 if __name__ == '__main__':
     fd = FaceDetector()
     X_train, X_test, _, _ = Indexer.load_split(folder_path=INDEXED_TTS_PATH)
     face_frame = fd.get_face_counts(X_train)
-    # write_to_file(os.path.join(STORED_FACE_PATH, 'train_faces.pkl'), face_frame)
+    #   write_to_file(os.path.join(STORED_FACE_PATH, 'train_faces.pkl'), face_frame)
     face_frame = fd.get_face_counts(X_test)
-# write_to_file(os.path.join(STORED_FACE_PATH, 'test_faces.pkl'), face_frame)
+#   write_to_file(os.path.join(STORED_FACE_PATH, 'test_faces.pkl'), face_frame)
