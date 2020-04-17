a47798e59c902fb9f869eb99e3ff805f88258748
AlexRaschl <Alexander.Raschl@aon.at>
2020-04-17 15:50:33 +0200

    Added face statistics to features.

--- src/preprocessing/detect/face.py
+++ src/preprocessing/detect/face.py
@@ -20,25 +20,35 @@ class FaceDetector:
     def get_face_counts(self, X: pd.DataFrame):
         v_ids = DataManager.extract_v_ids(X)
         face_info = np.array([self.detect_faces(v_id) for v_id in tqdm(v_ids)])
-        return pd.DataFrame({'v_id': v_ids,
-                             'n_faces': face_info[:, 0],
-                             'face_coverage': face_info[:, 1],
-                             'face_per_frame': face_info[:, 2]})
+        return pd.DataFrame(
+            {'v_id': v_ids,
+             'n_faces': face_info[:, 0],
+             'mean_faces': face_info[:, 1],
+             'min_faces': face_info[:, 2],
+             'max_faces': face_info[:, 3],
+             'q1_faces': face_info[:, 4],
+             'median_faces': face_info[:, 5],
+             'q3_faces': face_info[:, 6],
+             'face_coverage': face_info[:, 7]
+             }
+        )
 
     def detect_faces(self, v_id: str):
         images = self.load_batch(v_id)
 
         face_covered_area = 0.0
-        n_faces = 0
+        faces_per_frame = []
         for img in images:
             faces = self.face_cascade.detectMultiScale(img,
                                                        scaleFactor=1.1,
                                                        minNeighbors=5,
                                                        minSize=(25, 25))
 
-            n_faces += len(faces)
+            faces_per_frame.append(len(faces))
             face_covered_area += self.get_coverage(faces, img)
-        return n_faces, face_covered_area / len(images), n_faces / len(images)
+        faces = np.array(faces_per_frame)
+        return faces.sum(), faces.mean(), faces.min(), faces.max(), np.quantile(faces, q=0.25), \
+               np.median(faces), np.quantile(faces, q=0.75), face_covered_area / len(images)
 
     def load_batch(self, v_id: str) -> List[np.ndarray]:
         return [cv2.cvtColor(cv2.imread(path), cv2.COLOR_BGR2GRAY) for path in
@@ -55,6 +65,6 @@ if __name__ == '__main__':
     fd = FaceDetector()
     X_train, X_test, _, _ = Indexer.load_split(folder_path=INDEXED_TTS_PATH)
     face_frame = fd.get_face_counts(X_train)
-    #   write_to_file(os.path.join(STORED_FACE_PATH, 'train_faces.pkl'), face_frame)
+    #  write_to_file(os.path.join(STORED_FACE_PATH, 'train_faces.pkl'), face_frame)
     face_frame = fd.get_face_counts(X_test)
-#   write_to_file(os.path.join(STORED_FACE_PATH, 'test_faces.pkl'), face_frame)
+# write_to_file(os.path.join(STORED_FACE_PATH, 'test_faces.pkl'), face_frame)
